01bd1928f30cac73ac87e4a83a8d870364d66373
Melvin Zambrano <melvinzambrano641@gmail.com>
2026-05-13 00:22:21 -0500

    Arreglar archivos Django

---
+++ desarrollodesoftware/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from core import views as core_views
+from portafolio import views as portafolio_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', core_views.home, name='home'),
+    path('about/', core_views.about, name='about'),
+    path('contact/', core_views.contact, name='contact'),
+    path('portafolio/', portafolio_views.portafolio, name='portafolio'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
