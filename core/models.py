427c3329c3b6c54fe1025e923a51d7e50be2ced5
Melvin Zambrano <melvinzambrano641@gmail.com>
2026-05-17 21:49:45 -0500

    Modelo Persona y plantillas dinamicas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Persona(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    titulo_academico = models.CharField(max_length=150)
+    biografia = models.TextField()
+    correo_electronico = models.EmailField()
+    dedicacion = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
--- core/views.py
+++ core/views.py
@@ -1,28 +1,47 @@
 from django.shortcuts import render, HttpResponse
 from portafolio.models import Profile
+from .models import Persona
+
 
 def home(request):
 
     profile = Profile.objects.first()
+    persona = Persona.objects.first()
 
     return render(request, 'core/home.html', {
-        'profile': profile
+        'profile': profile,
+        'persona': persona
     })
+
 
 def about(request):
+
     profile = Profile.objects.first()
+    persona = Persona.objects.first()
+
     return render(request, "core/about.html", {
-        'profile': profile
+        'profile': profile,
+        'persona': persona
     })
+
 
 def portfolio(request):
+
     profile = Profile.objects.first()
+    persona = Persona.objects.first()
+
     return render(request, "core/../portafolio/templates/portafolio/portafolio.html", {
-        'profile': profile
+        'profile': profile,
+        'persona': persona
     })
 
+
 def contact(request):
+
     profile = Profile.objects.first()
+    persona = Persona.objects.first()
+
     return render(request, "core/contact.html", {
-        'profile': profile
+        'profile': profile,
+        'persona': persona
     })
